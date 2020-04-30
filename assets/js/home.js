1a7781d3ebbf4ff0ef5e80002c8e764cb9ec2bb2
greggailly <greggailly@yahoo.fr>
2020-04-30 09:47:06 +0200

    end of quill trial

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,5 +1,80 @@
-import $ from 'jquery';
+import Quill from 'quill'
+import ImageUploader from 'quill-image-uploader'
+import ImageResize from 'quill-image-resize-module'
 
-export default $("#clickMe").click(function send() {
-    console.log('clicked');
+import $ from 'jquery'
+
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.core.css'
+
+Quill.register({
+    'modules/imageUploader': ImageUploader,
+    'modules/imageResize': ImageResize
+})
+
+var toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+
+    ['image'],
+
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+    [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+
+    ['clean']                                         // remove formatting button
+];
+
+var options = {
+    theme: 'snow',
+    modules: {
+        toolbar: toolbarOptions,
+        imageResize: [],
+        imageUploader: {
+            upload: (file) => {
+                return new Promise((resolve, reject) => {
+                    const formData = new FormData()
+                    formData.append('image', file)
+
+                    $.ajax({
+                        url: '/image',
+                        method: 'POST',
+                        data: formData,
+                        processData: false,
+                        contentType: false
+                    }).then(response => {
+                        resolve(response.url)
+                    }).catch(error => {
+                        reject(error)
+                    })
+                });
+            },
+        }
+    }
+};
+
+var editor = new Quill('#editor-container', options)
+
+$('#send').click(function () {
+    var content = $('.ql-editor').html()
+    $.ajax({
+        url: '/send',
+        method: 'POST',
+        data: {
+            'content': content
+        }
+    }).then(response => {
+        alert('Success')
+    }).catch(error => {
+        alert('Error')
+    })
 })
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Kernel;
+use App\Repository\MessageRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -10,16 +17,58 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index()
+    public function index(MessageRepository $repo)
     {
-        return $this->render('home/index.html.twig');
+        $messages = $repo->findAll();
+        if (count($messages) == 0) {
+            $message = new Message();
+        } else {
+            $message = $messages[0];
+        }
+        return $this->render('home/index.html.twig', [
+            'message' => $message
+        ]);
     }
 
     /** 
      * @Route("/home", name="home")
      */
-    public function home()
+    public function home(MessageRepository $repo)
     {
-        return $this->render('home/home.html.twig');
+        $messages = $repo->findAll();
+        if (count($messages) == 0) {
+            $message = new Message();
+        } else {
+            $message = $messages[0];
+        }
+        return $this->render('home/home.html.twig', [
+            'message' => $message
+        ]);
+    }
+
+    /** @Route("/send", name="send") */
+    public function send(EntityManagerInterface $manager)
+    {
+        $message = new Message();
+        $message->setContent($_POST['content']);
+        $manager->persist($message);
+        $manager->flush();
+
+        return new JsonResponse();
+    }
+
+    /**
+     * @Route("/image", name="image")
+     */
+    public function image(Request $request)
+    {
+        /** @var UploadedFile $file */
+        $file = $request->files->get('image');
+
+        $destination = Kernel::getProjectDir() . '/public/uploads/images';
+        $originalFileName = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $movedFile = $file->move($destination, $originalFileName . '-' . uniqid() . '.' . $file->guessExtension());
+
+        return new JsonResponse(['url' => './uploads/images/' . $movedFile->getFilename()]);
     }
 }
